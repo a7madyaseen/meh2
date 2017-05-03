aa14eb12e36be99ecd787ee170e9f4af6c49a8ac
Ahmad.Yaseen <Ahmad.Yaseen@ahmadlaptop.aauj.edu>
2017-05-03 22:21:07 +0400

    second commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'hello';
+        echo 'hello22222';
         // put your code here
         ?>
     </body>
